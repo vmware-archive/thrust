734bf40b5a464a3526b49fdaee438c162987ca9f
Dominick Reinhold and Philip Kuryloski <pair+dreinhold+pkuryloski@pivotallabs.com>
2014-02-04 17:49:47 -0800

    dont load cedar rake tasks if in an android project

--- lib/thrust/tasks.rb
+++ lib/thrust/tasks.rb
@@ -1,3 +1,3 @@
-Dir[File.expand_path("../../tasks/*.rake", __FILE__)].each do |file|
-  load file
-end
+load File.expand_path("../../tasks/cedar.rake", __FILE__) unless File.exists?('AndroidManifest.xml')
+load File.expand_path("../../tasks/testflight.rake", __FILE__)
+load File.expand_path("../../tasks/version.rake", __FILE__)
