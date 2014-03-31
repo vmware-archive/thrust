bc24159d9e58a3fbf75c8f8f10b665598af342ca
Can Berk Güder <cbguder@pivotallabs.com>
2014-03-31 14:48:49 -0700

    Enable colored rspec output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,8 @@ require_relative '../lib/thrust'
 require_relative 'lib/fake_executor'
 
 RSpec.configure do |c|
+  c.color_enabled = true
+
   c.before(:all) { ENV['ANDROID_HOME'] = '/fake/android_home' }
   c.before(:each) { FakeFS.activate! }
   c.after(:each) { FakeFS.deactivate! }
