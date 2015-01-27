ed52795a96c0583b11a159635f56f1c214cfb9b9
Eugenia Dellapenna <edellapenna@pivotal.io>
2015-01-27 12:24:04 -0800

    Fixes issue where running a very short spec suite with ios-sim does not always print the spec output
    [fixes #87027636]

--- lib/thrust/spec_launcher.rb
+++ lib/thrust/spec_launcher.rb
@@ -16,15 +16,26 @@ module Thrust
         device_type_id = "com.apple.CoreSimulator.SimDeviceType.#{device_name}, #{os_version}"
         app_executable = File.join(build_dir, "#{build_configuration}-#{build_sdk}", "#{executable_name}.app")
         simulator_binary ||= 'ios-sim'
+        output_file = "tmp/thrust_specs_output"
 
         arguments = ["--devicetypeid \"#{device_type_id}\"",
                      "--timeout #{timeout || '30'}",
-                     "--setenv CFFIXED_USER_HOME=\"#{Dir.tmpdir}\""]
+                     "--stdout #{output_file}",
+                     "--setenv CFFIXED_USER_HOME=\"#{Dir.tmpdir}\"",]
+
         environment_variables.each do |key, value|
           arguments << "--setenv #{key}=\"#{value}\""
         end
 
-        @thrust_executor.check_command_for_failure("#{simulator_binary} launch #{app_executable} #{arguments.compact.join(' ')}")
+        @thrust_executor.system_or_exit("#{simulator_binary} launch #{app_executable} #{arguments.compact.join(' ')}")
+
+        results = File.read(output_file)
+        FileUtils.rm_r('tmp')
+
+        @out.puts 'Results:'
+        @out.puts results
+
+        results.include?("Finished") && !results.include?("FAILURE") && !results.include?("EXCEPTION")
       end
     end
   end
--- spec/lib/thrust/spec_launcher_spec.rb
+++ spec/lib/thrust/spec_launcher_spec.rb
@@ -16,85 +16,128 @@ describe Thrust::SpecLauncher do
   subject { Thrust::SpecLauncher.new(out, thrust_executor) }
 
   describe '#run' do
-    before do
-      allow(thrust_executor).to receive(:check_command_for_failure).and_return(true)
-    end
-
     def launch_specs
       subject.run(executable_name, build_configuration, build_sdk, os_version, device_name, timeout, build_dir, ios_sim_path, environment_variables)
     end
 
-    it 'launches the spec executable with ios-sim and returns true' do
-      success = launch_specs
+    describe 'running iOS specs' do
+      before do
+        allow(thrust_executor).to receive(:system_or_exit)
 
-      expect(success).to eq(true)
+        Dir.mkdir('tmp')
+        File.open('tmp/thrust_specs_output', 'w+') { |f| f.puts 'Finished in 0.0100 seconds 1 examples, 0 failures' }
+      end
 
-      expected_command = '/path/to/my/ios-sim launch build_dir/build_configuration-os/AwesomeExecutable.app --devicetypeid "com.apple.CoreSimulator.SimDeviceType.device-name, os-version"'
-      expect(thrust_executor).to have_received(:check_command_for_failure).with(/#{expected_command}/)
-    end
+      it 'launches the spec executable with ios-sim and returns true when the specs succeed' do
+        success = launch_specs
 
-    it 'passes timeout to ios-sim' do
-      launch_specs
+        expect(success).to eq(true)
 
-      expect(thrust_executor).to have_received(:check_command_for_failure).with(/--timeout 45/)
-    end
+        expected_command = '/path/to/my/ios-sim launch build_dir/build_configuration-os/AwesomeExecutable.app --devicetypeid "com.apple.CoreSimulator.SimDeviceType.device-name, os-version"'
+        expect(thrust_executor).to have_received(:system_or_exit).with(/#{expected_command}/)
+      end
 
-    it 'sets the CFFIXED_USER_HOME to a tmp directory' do
-      launch_specs
-
-      expect(thrust_executor).to have_received(:check_command_for_failure).with(/--setenv CFFIXED_USER_HOME="\/tmp"/)
-    end
-
-    it 'passes the custom environment variables to ios-sim' do
-      launch_specs
-
-      expect(thrust_executor).to have_received(:check_command_for_failure).with(/--setenv CEDAR_RANDOM_SEED="100"/)
-      expect(thrust_executor).to have_received(:check_command_for_failure).with(/--setenv CEDAR_REPORTER_OPTS="nested"/)
-    end
-
-    it 'returns false when the command fails' do
-      allow(thrust_executor).to receive(:check_command_for_failure).and_return(false)
-
-      expect(launch_specs).to be_falsey
-    end
-
-    context 'with macosx as the build_sdk' do
-      let(:build_sdk) { 'macosx' }
-
-      it 'should (safely) pass thrust the build path as an env variable' do
+      it 'passes timeout to ios-sim' do
         launch_specs
 
-        expect(thrust_executor).to have_received(:check_command_for_failure).with('"build_dir/build_configuration/AwesomeExecutable"', {'DYLD_FRAMEWORK_PATH' => '"build_dir/build_configuration"'})
+        expect(thrust_executor).to have_received(:system_or_exit).with(/--timeout 45/)
+      end
+
+      it 'sets the CFFIXED_USER_HOME to a tmp directory' do
+        launch_specs
+
+        expect(thrust_executor).to have_received(:system_or_exit).with(/--setenv CFFIXED_USER_HOME="\/tmp"/)
+      end
+
+      it 'passes the custom environment variables to ios-sim' do
+        launch_specs
+
+        expect(thrust_executor).to have_received(:system_or_exit).with(/--setenv CEDAR_RANDOM_SEED="100"/)
+        expect(thrust_executor).to have_received(:system_or_exit).with(/--setenv CEDAR_REPORTER_OPTS="nested"/)
+      end
+
+      it 'cleans up the spec output' do
+        launch_specs
+
+        expect(Dir.exists?('tmp')).to be_falsey
+      end
+
+      it 'returns false when the command fails with a test failure' do
+        File.open('tmp/thrust_specs_output', 'w+') { |f| f.puts 'FAILURE MyScene should fail MySceneSpec.mm:31 Expected <NO> to evaluate to true Finished in 0.0108 seconds 2 examples, 1 failures' }
+
+        expect(launch_specs).to be_falsey
+      end
+
+      it 'returns false when the command fails with an exception' do
+        File.open('tmp/thrust_specs_output', 'w+') { |f| f.puts 'EXCEPTION MyScene should blow up Finished in 0.0108 seconds 2 examples, 1 failures' }
+
+        expect(launch_specs).to be_falsey
+      end
+
+      it "returns false when the specs don't finish" do
+        File.open('tmp/thrust_specs_output', 'w+') { |f| f.puts '..' }
+
+        expect(launch_specs).to be_falsey
+      end
+
+      context 'when no ios_sim_path is provided' do
+        let(:ios_sim_path) { nil }
+
+        it 'defaults to system-installed ios-sim' do
+          launch_specs
+
+          expect(thrust_executor).to have_received(:system_or_exit).with(/ios-sim/)
+        end
+      end
+
+      context 'when no timeout is provided' do
+        let(:timeout) { nil }
+
+        it 'defaults to 30' do
+          launch_specs
+
+          expect(thrust_executor).to have_received(:system_or_exit).with(/--timeout 30/)
+        end
       end
     end
 
-    context 'with macosx-ish as the build_sdk' do
-      let(:build_sdk) { 'macosx10.9' }
-
-      it 'should (safely) pass thrust the build path as an env variable' do
-        launch_specs
-
-        expect(thrust_executor).to have_received(:check_command_for_failure).with('"build_dir/build_configuration/AwesomeExecutable"', {'DYLD_FRAMEWORK_PATH' => '"build_dir/build_configuration"'})
+    describe 'running Mac OSX specs' do
+      before do
+        allow(thrust_executor).to receive(:check_command_for_failure).and_return(true)
       end
-    end
 
-    describe 'when no ios_sim_path is provided' do
-      let(:ios_sim_path) { nil }
+      context 'with macosx as the build_sdk' do
+        let(:build_sdk) { 'macosx' }
 
-      it 'defaults to system-installed ios-sim' do
-        launch_specs
+        it 'launches the spec executable and checks for failure' do
+          success = launch_specs
 
-        expect(thrust_executor).to have_received(:check_command_for_failure).with(/ios-sim/)
+          expect(success).to be_truthy
+          expect(thrust_executor).to have_received(:check_command_for_failure).with('"build_dir/build_configuration/AwesomeExecutable"', {'DYLD_FRAMEWORK_PATH' => '"build_dir/build_configuration"'})
+        end
+
+        it 'returns false when the command fails' do
+          allow(thrust_executor).to receive(:check_command_for_failure).and_return(false)
+
+          expect(launch_specs).to be_falsey
+        end
       end
-    end
 
-    describe 'when no timeout is provided' do
-      let(:timeout) { nil }
+      context 'with macosx-ish as the build_sdk' do
+        let(:build_sdk) { 'macosx10.9' }
 
-      it 'defaults to 30' do
-        launch_specs
+        it 'launches the spec executable and checks for failure' do
+          success = launch_specs
 
-        expect(thrust_executor).to have_received(:check_command_for_failure).with(/--timeout 30/)
+          expect(success).to be_truthy
+          expect(thrust_executor).to have_received(:check_command_for_failure).with('"build_dir/build_configuration/AwesomeExecutable"', {'DYLD_FRAMEWORK_PATH' => '"build_dir/build_configuration"'})
+        end
+
+        it 'returns false when the command fails' do
+          allow(thrust_executor).to receive(:check_command_for_failure).and_return(false)
+
+          expect(launch_specs).to be_falsey
+        end
       end
     end
   end
